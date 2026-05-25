80204918b49874a98532ceddc1bb8f5f2289238b
xiangfeidexiaohuo <jsjson@163.com>
2026-05-26 01:10:05 +0800

    airplay2
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- op-airpaly2/README.md
+++
@@ -1,3 +0,0 @@
-# luci-app-airplay2 (shairport-sync)
-
-AirPlay 2 is a simple and easy-to-use AirPlay audio player.
--- op-airpaly2/luci-app-airplay2/htdocs/luci-static/resources/view/airplay2.js
+++ op-airpaly2/luci-app-airplay2/htdocs/luci-static/resources/view/airplay2.js
@@ -12,6 +12,20 @@ var callServiceList = rpc.declare({
 	expect: { '': {} }
 });
 
+var callFileStat = rpc.declare({
+	object: 'file',
+	method: 'stat',
+	params: ['path'],
+	expect: { '': {} }
+});
+
+var callExec = rpc.declare({
+	object: 'file',
+	method: 'exec',
+	params: ['command', 'params'],
+	expect: {}
+});
+
 function getServiceStatus() {
 	return L.resolveDefault(callServiceList('airplay2'), {}).then(function (res) {
 		var isRunning = false;
@@ -30,82 +44,138 @@ function renderStatus(isRunning) {
 	} else {
 		renderHTML = spanTemp.format('red', 'Airplay2', _('NOT RUNNING'));
 	}
-
 	return renderHTML;
 }
 
-return view.extend({
-	render: function() {
-		var m, s, o;
-
-		m = new form.Map('airplay2', _('Airplay 2'),
-			_('AirPlay 2 is a simple and easy-to-use AirPlay audio player.'));
-
-		s = m.section(form.TypedSection);
-		s.anonymous = true;
-		s.render = function () {
-			poll.add(function () {
-				return L.resolveDefault(getServiceStatus()).then(function (res) {
-					var view = document.getElementById('service_status');
-					view.innerHTML = renderStatus(res);
-				});
+function getSoundCards() {
+	var alsaPromise = callExec('/bin/cat', ['/proc/asound/cards']).then(function (res) {
+		var content = res && typeof res.stdout === 'string' ? res.stdout : '';
+		var cards = [];
+		if (content) {
+			var lines = content.split('\n');
+			lines.forEach(function (line) {
+				var match = line.match(/^\s*(\d+)\s+\[[^\]]+\]:\s*(.+)$/);
+				if (match) {
+					cards.push({
+						type: 'alsa',
+						id: match[1],
+						desc: match[2]
+					});
+				}
 			});
-
-			return E('div', { class: 'cbi-section', id: 'status_bar' }, [
-					E('p', { id: 'service_status' }, _('Collecting data...'))
-			]);
 		}
+		return cards;
+	}).catch(function (e) {
+		return [];
+	});
 
-		s = m.section(form.NamedSection, '@airplay2[0]', 'airplay2');
+	var bluetoothPromise = callFileStat('/usr/bin/bluealsa-aplay').then(function(stat) {
+		if (stat) {
+			return callExec('/usr/bin/bluealsa-aplay', ['-l']).then(function(res) {
+				var devices = [];
+				if (res.code === 0 && res.stdout) {
+					res.stdout.trim().split('\n').forEach(function(line) {
+						if (line.indexOf('audio') !== -1) {
+							var match = line.match(/([0-9A-F]{2}(:[0-9A-F]{2}){5}).*\[([^\]]+)\]/i);
+							if (match) {
+								devices.push({ type: 'bluealsa', id: match[1], desc: match[3] });
+							}
+						}
+					});
+				}
+				return devices;
+			});
+		}
+		return [];
+	}).catch(function(e) {
+		return [];
+	});
 
-		o = s.option(form.Flag, 'enabled', _('Enabled'));
-		o.rmempty = false;
+	return Promise.all([alsaPromise, bluetoothPromise]).then(function(results) {
+		return results[0].concat(results[1]);
+	});
+}
 
-		o = s.option(form.Value, 'name', _('Airplay Name'));
-		o.rmempty = false;
+return view.extend({
+	render: function () {
+		return getSoundCards().then(function (cards) {
+			var m = new form.Map('airplay2', _('Airplay 2'),
+				_('AirPlay 2 is a simple and easy-to-use AirPlay audio player.'));
 
-		o = s.option(form.ListValue, 'interpolation', _('Interpolation'));
-		o.default = 'basic';
-		o.value('basic', _('Internal Basic Resampler'));
-		o.value('soxr', _('High quality SoX Resampler'));
+			var s = m.section(form.TypedSection);
+			s.anonymous = true;
+			s.render = function () {
+				poll.add(function () {
+					return L.resolveDefault(getServiceStatus()).then(function (res) {
+						var view = document.getElementById('service_status');
+						view.innerHTML = renderStatus(res);
+					});
+				});
+				return E('div', { class: 'cbi-section', id: 'status_bar' }, [
+					E('p', { id: 'service_status' }, _('Collecting data...'))
+				]);
+			};
 
-		o = s.option(form.Value, 'port', _('Port'));
-		o.default = '5050';
-		o.datatype = 'port';
+			s = m.section(form.NamedSection, '@airplay2[0]', 'airplay2');
+			s.anonymous = true;
 
-		o = s.option(form.ListValue, 'alsa_output_device', _('Alsa Output Device'));
-		o.default = '';
-		o.value('', _('default'));
-		o.value('hw:0', _('1st Soundcard (hw:0)'));
-		o.value('hw:0,3', _('1st Soundcard 4th device (hw:0,3)'));
-		o.value('hw:1', _('2nd Soundcard (hw:1)'));
-		o.value('hw:2', _('3rd Soundcard (hw:2)'));
+			var o;
 
-		o = s.option(form.ListValue, 'alsa_mixer_control_name', _('Alsa Mixer Control Name'));
-		o.default = '';
-		o.value('', _('default (software volume)'));
-		o.value('PCM', _('PCM'));
-		o.value('Speaker', _('Speaker'));
+			o = s.option(form.Flag, 'enabled', _('Enabled'));
+			o.rmempty = false;
 
-		o = s.option(form.ListValue, 'alsa_output_rate', _('Alsa Output Rate'));
-		o.default = 'auto';
-		o.value('auto', _('auto'));
-		o.value('44100', _('44.1kHz'));
-		o.value('88200', _('88.2kHz'));
-		o.value('176400', _('176.4kHz'));
-		o.value('352800', _('352.8kHz'));
+			o = s.option(form.Value, 'name', _('Airplay Name'));
+			o.rmempty = false;
 
-		o = s.option(form.Value, 'alsa_buffer_length', _('Alsa Buffer Length'));
-		o.default = '6615';
+			o = s.option(form.ListValue, 'interpolation', _('Interpolation'));
+			o.default = 'basic';
+			o.value('basic', _('Internal Basic Resampler'));
+			o.value('soxr', _('High quality SoX Resampler'));
 
-		o = s.option(form.Value, 'sesctl_session_timeout', _('Session timeout'));
-		o.default = '120';
+			o = s.option(form.Value, 'port', _('Port'));
+			o.default = '5050';
+			o.datatype = 'port';
 
-		o = s.option(form.ListValue, 'sesctl_session_interruption', _('Allow session interruption'));
-		o.default = 'no';
-		o.value('no', _('Not allow'));
-		o.value('yes', _('Allow'));
+			var soundcardOpt = s.option(form.ListValue, 'alsa_output_device', _('Alsa Output Device'));
+			soundcardOpt.default = '';
+			soundcardOpt.value('', _('default'));
+			soundcardOpt.value('bluealsa', _('Default Bluetooth Audio (BlueALSA)'));
+			if (Array.isArray(cards)) {
+				cards.forEach(function (card) {
+					if (card.type === 'bluealsa') {
+						soundcardOpt.value('bluealsa:DEV=' + card.id, card.desc + ' (' + card.id + ')');
+					} else {
+						soundcardOpt.value('hw:' + card.id, card.desc + ' (hw:' + card.id + ')');
+					}
+				});
+			}
 
-		return m.render();
+			o = s.option(form.ListValue, 'alsa_mixer_control_name', _('Alsa Mixer Control Name'));
+			o.default = '';
+			o.value('', _('default (software volume)'));
+			o.value('PCM', _('PCM'));
+			o.value('Speaker', _('Speaker'));
+
+			o = s.option(form.ListValue, 'alsa_output_rate', _('Alsa Output Rate'));
+			o.default = 'auto';
+			o.value('auto', _('auto'));
+			o.value('44100', _('44.1kHz'));
+			o.value('88200', _('88.2kHz'));
+			o.value('176400', _('176.4kHz'));
+			o.value('352800', _('352.8kHz'));
+
+			o = s.option(form.Value, 'alsa_buffer_length', _('Alsa Buffer Length'));
+			o.default = '6615';
+
+			o = s.option(form.Value, 'sesctl_session_timeout', _('Session timeout'));
+			o.default = '120';
+
+			o = s.option(form.ListValue, 'sesctl_session_interruption', _('Allow session interruption'));
+			o.default = 'no';
+			o.value('no', _('Not allow'));
+			o.value('yes', _('Allow'));
+
+			return m.render();
+		});
 	}
 });
